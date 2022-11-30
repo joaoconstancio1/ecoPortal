472cb7725b2f1d96c0107ade2d14fdec74ad0b9d
Joao Vitor <joaovitorconstancio@hotmail.com>
2022-11-29 23:23:31 -0300

    showing reviews

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -33,120 +33,126 @@ class _DetailsPageState extends State<DetailsPage> {
       backgroundColor: Colors.grey[600],
       appBar: CustomAppBar(title: data?.title ?? ''),
       body: SafeArea(
-        child: Stack(
+        child: ListView(
           children: [
-            ListView(
+            Stack(
               children: [
-                Stack(
-                  children: [
-                    Container(
-                      foregroundDecoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Colors.transparent,
-                            Color(0xff0e0f11),
-                          ],
-                          begin: Alignment(0, 0.3),
-                          end: Alignment.bottomCenter,
-                        ),
-                      ),
-                      child: Container(
-                          height: 400,
-                          width: double.infinity,
-                          child: Image.network(
-                            data?.imgUrl ?? "",
-                            fit: BoxFit.fill,
-                          )),
+                Container(
+                  foregroundDecoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Color(0xff0e0f11),
+                      ],
+                      begin: Alignment(0, 0.3),
+                      end: Alignment.bottomCenter,
                     ),
-                  ],
+                  ),
+                  child: Container(
+                      height: 400,
+                      width: double.infinity,
+                      child: Image.network(
+                        data?.imgUrl ?? "",
+                        fit: BoxFit.fill,
+                      )),
                 ),
+                Positioned(
+                  top: 300,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    width: 400,
+                    child: Text(
+                      data?.title ?? 'Movie title',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-            Positioned.fill(
-              top: 300,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Text(
-                        data?.title ?? 'Movie title',
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Release date: ${data?.releaseDate ?? ''}',
+                    style: const TextStyle(
+                      color: Colors.orangeAccent,
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.yellowAccent,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '${_averageRating()} / 5',
                         style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 32,
+                          color: Colors.orangeAccent,
+                          fontSize: 16,
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Release date: ${data?.releaseDate ?? ''}',
-                            style: const TextStyle(
-                              color: Colors.orangeAccent,
-                            ),
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              const Icon(
-                                Icons.star,
-                                color: Colors.yellowAccent,
-                                size: 20,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                '${_averageRating()} / 5',
-                                style: const TextStyle(
-                                  color: Colors.orangeAccent,
-                                  fontSize: 16,
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
-            // Positioned(
-            //   top: 450,
-            //   child: ListView.builder(
-            //     padding: EdgeInsets.all(10),
-            //     itemCount: reviewsData?.length,
-            //     itemBuilder: (context, index) {
-            //       return Card(
-            //         shape: RoundedRectangleBorder(
-            //           borderRadius: BorderRadius.circular(10.0),
-            //         ),
-            //         elevation: 8,
-            //         child: Container(
-            //             padding: EdgeInsets.all(10),
-            //             child: Column(
-            //               children: [
-            //                 Row(
-            //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //                   children: [
-            //                     Text(reviewsData?[index].title ?? 'title'),
-            //                     Text(reviewsData?[index].rating.toString() ??
-            //                         'title')
-            //                   ],
-            //                 ),
-            //                 Text(reviewsData?[index].title ?? 'title'),
-            //               ],
-            //             )),
-            //       );
-            //     },
-            //   ),
-            // )
+            ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10),
+              itemCount: reviewsData?.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  elevation: 8,
+                  child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(reviewsData?[index].title ?? 'title'),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    reviewsData?[index].rating.toString() ?? '',
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.yellowAccent,
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Text(reviewsData?[index].body ?? ''),
+                        ],
+                      )),
+                );
+              },
+            )
           ],
         ),
       ),
